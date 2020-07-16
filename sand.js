bd4b7ed1fa3def7ca8aa298d687a4a401feaceca
Siddhant Singh <siddhant.singh.2914@gmail.com>
2020-07-16 17:15:26 +0530

    Modified Javascript added variable with different datatypes

--- sand.js
+++ sand.js
@@ -1,2 +1,11 @@
-var firstName = "Rakesh"
-console.log(firstName);
+var firstName = "Rakesh"; // String
+console.log(firstName); // Prints on chrome console
+
+var lastName = "Singh";
+console.log(firstName + " "+lastName); 
+
+var isAge = true; // Boolean 
+var num = 1; // Number
+var und; // Undefined
+var _und = null ;// non exsitent
+
